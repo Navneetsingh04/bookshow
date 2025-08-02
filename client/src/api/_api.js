0050094640b5df41ceb2847cc0743be76222b2d2
Navneet Singh <navneetsingh1825@gmail.com>
2025-08-02 22:40:27 +0530

    error fix

--- client/src/api/_api.js
+++ client/src/api/_api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL:  import.meta.env.VITE_API_URL + "/api",
+  baseURL: "http://localhost:4001/api",
   headers: {
     "Content-Type": "application/json",
   },
